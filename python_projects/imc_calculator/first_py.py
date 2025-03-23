8d069b013502ff9d786b6dc2677b1e3260696771
RomeuDev <romeuarrudadev@gmail.com>
2025-03-23 20:53:57 -0300

    interface ASCII adicionada no código python

--- python_projects/imc_calculator/first_py.py
+++ python_projects/imc_calculator/first_py.py
@@ -1,8 +1,34 @@
+print("""
+      Olá! A classificação dessa calculadora é 
+      realizada a partir da tabela fornecida pela 
+      Organização Mundial da Saúde (OMS):
+
++-------------------+-------------------+
+|       IMC         |    Diagnóstico    |
++-------------------+-------------------+
+| menor que 18,5    | baixo peso        |
+| entre 18,5 e 24,9 | intervalo normal  |
+| entre 25 e 29,9   | sobrepeso         |
+| entre 30 e 34,9   | obesidade classe I|
++-------------------+-------------------+
+      """)
+
 def calcular_imc(peso, altura):
     imc = peso / (altura**2)
     return imc
 
-peso = float(input("Digite seu peso (kg): "))
-altura = float(input("Digite sua altura (em metros): "))
+# Função para substituir vírgula por ponto e converter para float
+def formatar_entrada(valor):
+    return float(valor.replace(",", "."))
+
+# Solicitar peso e altura
+peso = input("Digite seu peso em kg: ")
+altura = input("Digite sua altura em metros: ")
+
+# Converter as entradas para float, substituindo vírgula por ponto
+peso = formatar_entrada(peso)
+altura = formatar_entrada(altura)
+
+# Calcular e exibir o IMC
 imc = calcular_imc(peso, altura)
 print(f"Seu IMC é: {imc:.2f}")
